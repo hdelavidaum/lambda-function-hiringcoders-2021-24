1555e1d4bf56c554ca284bd0b613557b4e36a3ec
Henrique Delavi Daum <henrique.delavi.daum@gmail.com>
2021-08-28 12:03:36 -0300

    feat: getting all leads, lead by email, and posting a lead

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ AWS.config.update({
     region: "us-east-2",
 });
 
-const dynamo = new AWS.DynamoDB.DocumentClient();
+const ddb = new AWS.DynamoDB.DocumentClient();
 
 const tableName = "hiringcoders-2021-24-corebiz";
-const leadsPath = "/leads";
+
+const leadsResource = "/leads";
+const leadResource = "/leads/{id}";
+
 // const clientsPath = "/clients";
 // const prospectsPath = "/prospects";
 
@@ -15,7 +18,54 @@ function getLeads() {
         TableName: tableName,
     };
 
-    return dynamo.scan(params).promise();
+    return ddb.scan(params).promise();
+}
+
+function getLeadByEmail(email) {
+    const params = {
+        TableName: tableName,
+        Key: {
+            userEmail: email,
+        },
+    };
+
+    return ddb.get(params).promise();
+}
+
+function putLead(requestBody) {
+    const today = new Date();
+
+    const params = {
+        TableName: tableName,
+        Item: {
+            userEmail: requestBody.userEmail,
+            userType: requestBody.userType,
+            clientSince: requestBody.clientSince,
+            lastModified: requestBody.lastModified,
+            createdAt: today.toLocaleDateString("en-CA"),
+            phone: requestBody.phone,
+            name: requestBody.name,
+        },
+    };
+
+    return ddb.put(params).promise();
+}
+
+function updateLead(key, requestBody) {
+    const today = new Date();
+
+    const params = {
+        TableName: tableName,
+        Key: {
+            userEmail: key,
+        },
+        Item: {
+            ...requestBody,
+            lastModified: today.toLocaleDateString("en-CA"),
+        },
+    };
+
+    return ddb.update(params).promise();
 }
 
 function makeResponse(statusCode, body) {
@@ -28,23 +78,28 @@ function makeResponse(statusCode, body) {
     };
 }
 
-function getLeadByEmail(email) {
-    const params = {
-        TableName: tableName,
-        Key: {
-            userEmail: email,
-        },
-    };
-
-    return dynamo.get(params).promise();
-}
-
 exports.handler = async function (event, context, callback) {
+    let reqBody;
     let response;
 
     switch (true) {
+        // PATH /leads
+        case event.httpMethod === "GET" && event.path === leadsResource:
+            const leads = await getLeads();
+            response = makeResponse(200, leads);
+            break;
+
+        case event.httpMethod === "POST" && event.path === leadsResource:
+            reqBody = JSON.parse(event.body);
+            await putLead(reqBody);
+
+            response = makeResponse(200, {
+                message: `Lead with e-mail ${reqBody.userEmail} created with success`,
+            });
+            break;
+
         case event.httpMethod === "GET" &&
-            event.resource === "/leads/{id}" &&
+            event.resource === leadResource &&
             !!event.pathParameters.id:
             const lead = await getLeadByEmail(event.pathParameters.id).then(
                 (data) => data.Item
@@ -52,16 +107,22 @@ exports.handler = async function (event, context, callback) {
             response = makeResponse(200, lead);
             break;
 
-        case event.httpMethod === "GET" && event.path === leadsPath:
-            const leads = await getLeads();
-            response = makeResponse(200, leads);
+        case event.httpMethod === "PATCH" &&
+            event.resource === leadResource &&
+            !!event.pathParameters.id:
+            reqBody = JSON.parse(event.body);
+            await updateLead(event.pathParameters.id, reqBody);
+
+            response = makeResponse(200, {
+                message: `Lead "${
+                    event.pathParameters.id
+                }" has been updated this data: ${Object.values(reqBody)}`,
+            });
             break;
 
         default:
-            response = makeResponse(
-                400,
-                "não deu bom no leads nem com o email de ID"
-            );
+            response = makeResponse(400, { message: "" });
+            break;
     }
 
     return response;
